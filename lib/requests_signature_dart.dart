1e2d51182a99d286140e095125c292aace38ca9d
Thorvalld <anis.bejaoui@esprit.tn>
2024-03-19 14:08:23 +0100

    refactor: minor comments formatting

--- lib/requests_signature_dart.dart
+++ lib/requests_signature_dart.dart
@@ -1,22 +1,22 @@
 library requests_signature_dart;
 
-// Export exceptions
+/// Export exceptions
 export 'src/core/exception/requests_signature_exception.dart';
 
-// Export core implementation
+/// Export core implementation
 export 'src/core/implementation/hash_algorithm_signature_body_signer.dart';
 export 'src/core/implementation/signature_body_parameters.dart';
 export 'src/core/implementation/signature_body_source_builder.dart';
 export 'src/core/implementation/signature_body_source_components.dart';
 export 'src/core/implementation/signature_body_source_parameters.dart';
 
-// Export interfaces
+/// Export interfaces
 export 'src/core/interface/signature_body_signer.dart';
 export 'src/core/interface/signature_body_source_builder.dart';
 
-// Export default constants
+/// Export default constants
 export 'src/core/default_constants.dart';
 
-// Export client implementation
-///Client: Dio
+/// Export client implementation
+/// Client: Dio
 export 'src/client/dio_interceptor.dart';
