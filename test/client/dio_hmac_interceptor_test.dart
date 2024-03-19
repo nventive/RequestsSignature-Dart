f0e346e8bf5813dcad9d3b375daf3b4f70e69e73
Thorvalld <anis.bejaoui@esprit.tn>
2024-03-19 03:16:47 +0100

    test: refactored dio interceptor test and dropped mockito

--- test/client/dio_hmac_interceptor_test.dart
+++ test/client/dio_hmac_interceptor_test.dart
@@ -1,18 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:test/test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:requests_signature_dart/requests_signature_dart.dart';
-
-class MockDio extends Mock implements Dio {}
 
 void main() {
   group('HMACDioInterceptor', () {
-    test('Interceptor adds authentication headers correctly', () async {
+    test('Interceptor signs the request with authentication headers', () async {
       // Arrange
-      final dio = MockDio();
-      final clientId = "your_client_id";
-      final clientSecret = "your_client_secret";
-      final interceptor = HMACDioInterceptor(clientId, clientSecret);
+      final dio = Dio();
+      final interceptor =
+          HMACDioInterceptor("your_client_id", "your_client_secret");
+      dio.interceptors.add(interceptor);
 
       final options = RequestOptions(
         method: 'GET',
@@ -21,15 +18,11 @@ void main() {
         headers: {'Content-Type': 'application/json'},
       );
 
-      when(dio.request(options.baseUrl, options: anyNamed('options')))
-          .thenAnswer((_) async =>
-              Response(data: '', statusCode: 200, requestOptions: options));
-
       // Act
       await interceptor.onRequest(options, RequestInterceptorHandler());
 
       // Assert
-      expect(options.headers['X-RequestSignature-ClientId'], clientId);
+      expect(options.headers['X-RequestSignature-ClientId'], "your_client_id");
       expect(options.headers['X-RequestSignature-Nonce'], isNotEmpty);
       expect(options.headers['X-RequestSignature-Timestamp'], isNotEmpty);
       expect(options.headers['X-RequestSignature-Signature'], isNotEmpty);
